eb806429838c84c7ccb79d758e94ff4a0c3972a9
therese58 <therese.luce9@gmail.com>
2024-09-26 11:01:56 +0900

    동적객체 출력

--- src/basic.js
+++ src/basic.js
@@ -2,19 +2,13 @@ window.addEventListener('load', function(){
 // 호출영역
     this.document.querySelector("#family h2").innerHTML = family.d1text;
 
-    const mylist = this.document.querySelectorAll("#family li");
+    let familylist = '';
 
-    console.log(
-       `정체파악 : ${mylist}` ,  // [object, object, object]
-       `데이터타입 : ${typeof mylist}`, 
-       // object -> array도 object에 해당됨
-       `배열인가? ${Array.isArray(mylist)}`
-       // 배열인지 아닌지 확인하기 true, false로 출력됨
-       )
+    familylist += `<li>${family.familysite[0].atext}</li>`;
+    familylist += `<li>${family.familysite[1].atext}</li>`;
+    familylist += `<li>${family.familysite[2].atext}</li>`;
 
-       for (자리 in mylist){
-        mylist[자리].innerHTML = family.familysite[자리].atext;
-       } 
+    this.document.querySelector("#family ul").innerHTML = familylist;
 })
 
 // 함수제작
