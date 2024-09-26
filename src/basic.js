594e243312554042af011113b78b59300bb63d99
therese58 <therese.luce9@gmail.com>
2024-09-26 11:46:28 +0900

    패밀리사이트 스타일 구현

--- src/basic.js
+++ src/basic.js
@@ -1,14 +1,24 @@
 window.addEventListener('load', function(){
-// 호출영역
-    this.document.querySelector("#family h2").innerHTML = family.d1text;
+  //호출영역
+  this.document.querySelector("#family h2").innerHTML = family.d1text;
 
-    let familylist = '';
+  let familylist  = '';
 
-    familylist += `<li>${family.familysite[0].atext}</li>`;
-    familylist += `<li>${family.familysite[1].atext}</li>`;
-    familylist += `<li>${family.familysite[2].atext}</li>`;
+  // for(x in family.faimilysite ){
+  //     familylist += `<li>${family.faimilysite[x].atext}</li>`;
+  // }
 
-    this.document.querySelector("#family ul").innerHTML = familylist;
+  for(x of family.familysite ){
+    familylist += `<li>${x.atext}</li>`;
+  }
+  this.document.querySelector("#family ul").innerHTML = familylist;
+
+
+  //패밀리노출
+  this.document.querySelector("#family").addEventListener('click', function(){
+    this.classList.toggle('open');
+  })
+
 })
 
 // 함수제작
